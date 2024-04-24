c736b729dd14d50b6130205257a864bb5bb92aa6
ConfingLand <167057199+ConfingLand@users.noreply.github.com>
2024-04-24 20:09:33 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -166,7 +166,7 @@ async function vlessOverWSHandler(request) {
 				hasError,
 				message,
 				portRemote = [443, 8443, 2053, 2083, 2087, 2096, 80, 8080, 8880, 2052, 2082, 2086, 2095],
-				addressRemote = '144.76.153.48',
+				addressRemote = '',
 				rawDataIndex,
 				vlessVersion = new Uint8Array([0, 0]),
 				isUDP,
